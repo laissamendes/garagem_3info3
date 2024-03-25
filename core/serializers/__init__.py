8a378437f13b1e4fe919984340082bbb4be1249b
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-03-25 15:05:03 -0300

    Criação do projeto

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,6 @@
 from .user import UserSerializer
+from .acessorio import AcessorioSerializer
+from .categoria import CategoriaSerializer
+from .cor import CorSerializer
+from .marca import MarcaSerializer
+
